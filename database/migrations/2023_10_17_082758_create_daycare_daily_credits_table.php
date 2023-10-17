5c3d6ea19cdd054f7d2da4afe0a41d72e5fd4ef0
sistema7@engepecas.com.br <sistema7@engepecas.com.br>
2023-10-17 08:43:56 -0300

    construction

---
+++ database/migrations/2023_10_17_082758_create_daycare_daily_credits_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daycare_daily_credits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('daycare_enrollment_id');
+            $table->integer('daily_credit');
+            $table->date('validity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daycare_daily_credits');
+    }
+};
