495a53dd6f55bf74299300f258575a96cef6750e
sistema7@engepecas.com.br <sistema7@engepecas.com.br>
2023-12-26 14:52:02 -0300

    construction

--- app/Livewire/Admin/BathAndGrooming/Control/ModalShowBathService.php
+++ app/Livewire/Admin/BathAndGrooming/Control/ModalShowBathService.php
@@ -3,40 +3,53 @@
 namespace App\Livewire\Admin\BathAndGrooming\Control;
 
 use App\Services\BathAndGroomingBookingService;
+use Illuminate\Support\Carbon;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ModalShowBathService extends Component
 {
+    public $petName;
+
+    public $urlImage;
+
+    public $tutor;
+
     public $date;
 
     public $time;
 
-    public $bathType;
+    public $bathTypes = [];
+
+    public $bathComplements = [];
 
     public $booking;
 
+    public $extraServices;
+
+    public $notes;
+
     public function render()
     {
-        $petName = '';
-        $urlImage = '';
-        $tutor = '';
-        $bathType = [];
-        if ($this->booking) {
-            $petName = $this->booking->pet->name;
-            $urlImage = $this->booking->pet->image;
-            $tutor = $this->booking->pet->user->name;
-            $bathType = json_decode($this->booking->bath_type);
-        }
 
-        return view('livewire.admin.bath-and-grooming.control.modal-show-bath-service', compact('petName', 'urlImage', 'tutor'));
+        return view('livewire.admin.bath-and-grooming.control.modal-show-bath-service');
     }
 
     #[On('showBookingBath')]
-    public function getBath($id)
+    public function getBath(?int $id)
     {
 
-        $this->booking = app()->make(BathAndGroomingBookingService::class)->find($id);
-
+        if ($id) {
+            $this->booking = app()->make(BathAndGroomingBookingService::class)->find($id);
+            $this->petName = $this->booking->pet->name;
+            $this->urlImage = $this->booking->pet->image;
+            $this->date = Carbon::parse($this->booking->bath_date)->format('d/m/Y');
+            $this->time = Carbon::parse($this->booking->bath_time)->format('HH:ii');
+            $this->tutor = $this->booking->pet->user->name;
+            $this->bathTypes = json_decode($this->booking->bath_type);
+            $this->bathComplements = json_decode($this->booking->bath_complement);
+            $this->extraServices = $this->booking->extra_services;
+            $this->notes = $this->booking->notes;
+        }
     }
 }
--- resources/views/livewire/admin/bath-and-grooming/control/modal-show-bath-service.blade.php
+++ resources/views/livewire/admin/bath-and-grooming/control/modal-show-bath-service.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div wire:ignore.defer data-te-modal-init
+    <div wire:ignore.self data-te-modal-init
         class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
         id="showBathBooking" tabindex="-1" aria-labelledby="showBathBookingLabel" aria-modal="true" role="dialog">
-        <div data-te-modal-dialog-ref
+        <div data-te-modal-dialog-ref wire:ignore.self
             class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px] min-[992px]:max-w-[800px]">
             <div
                 class="relative flex flex-col w-full text-current bg-white border-none rounded-md shadow-lg outline-none pointer-events-auto bg-clip-padding dark:bg-neutral-600">
@@ -23,98 +23,107 @@
                         </svg>
                     </button>
                 </div>
-                <form wire:submit='booking'>
-                    <!--Modal body-->
+
+                <!--Modal body-->
+                <div class="relative flex-auto p-4" data-te-modal-body-ref wire:poll.5s>
                     <div class="relative flex-auto p-4" data-te-modal-body-ref>
-                        <div class="relative flex-auto p-4" data-te-modal-body-ref>
 
-                            <section class="p-6 text-center rounded-md shadow-lg md:p-12 md:text-left"
-                                style="background:#81D8D0">
-                                <div class="flex justify-center">
-                                    <div class="w-full">
-                                        <div
-                                            class="block p-6 m-4 bg-white rounded-lg shadow-lg dark:bg-neutral-800 dark:shadow-black/20">
-                                            <!--Testimonial-->
-                                            <div class="md:flex md:flex-row">
-                                                <div
-                                                    class="flex items-center justify-center mx-auto mb-6 w-36 md:mx-0 md:w-96 lg:mb-0">
-                                                    <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg"
-                                                        class="rounded-full shadow-md dark:shadow-black/30"
-                                                        alt="woman avatar" />
-                                                </div>
-                                                <div class="md:ml-6">
+                        <section class="p-6 text-center rounded-md shadow-lg md:p-12 md:text-left"
+                            style="background:#81D8D0">
+                            <div class="flex justify-center">
+                                <div class="w-full">
+                                    <div
+                                        class="block p-3 m-2 bg-white rounded-lg shadow-lg dark:bg-neutral-800 dark:shadow-black/20">
 
-                                                    <p
-                                                        class="mb-2 text-xl font-semibold text-neutral-800 dark:text-neutral-200">
-                                                        {{ $petName }}
-                                                    </p>
-                                                    <p
-                                                        class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
-                                                        {{ $tutor }}
-                                                    </p><br />
-                                                    <p>
+                                        <div class="md:flex md:flex-row">
+                                            <div
+                                                class="flex items-center justify-center mx-auto mb-6 w-28 md:mx-0 md:w-96 lg:mb-0">
+                                                @if ($urlImage)
+                                                    <img src="{{ asset('storage/' . $urlImage) }}"
+                                                        class="mx-auto mb-4 border-4 rounded-full shadow-lg dark:shadow-black/20"
+                                                        alt="" style="max-width: 200px" />
+                                                @else
+                                                    <img src="{{ asset('assets/images/canva.png') }}"
+                                                        class="mx-auto mb-4 border-4 rounded-full shadow-lg dark:shadow-black/20"
+                                                        alt="" style="max-width: 100px" />
+                                                @endif
+                                            </div>
+                                            <div class="md:ml-6">
 
-                                                    <div class="flex flex-col">
-                                                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                                            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                                                                <div class="overflow-hidden">
-                                                                    <table
-                                                                        class="min-w-full text-sm font-light text-left">
-
-                                                                        <tbody>
+                                                <p
+                                                    class="mb-2 text-xl font-semibold text-neutral-800 dark:text-neutral-200">
+                                                    {{ $petName }}
+                                                </p>
+                                                <p class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
+                                                    {{ $tutor }}
+                                                </p><br />
+                                                <p class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
+                                                    {{ $date }} - {{ $time }}
+                                                </p><br />
+                                                <div class="flex flex-col">
+                                                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                                        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                                                            <div class="overflow-hidden">
+                                                                <table class="min-w-full text-sm font-light text-left">
+                                                                    <tbody>
+                                                                        @foreach ($bathTypes as $bathType)
                                                                             <tr
                                                                                 class="transition duration-300 ease-in-out border-b hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
                                                                                 <td
                                                                                     class="px-6 py-4 font-medium whitespace-nowrap">
-                                                                                    1</td>
+                                                                                    <i
+                                                                                        class="text-green-500 fa-solid fa-check"></i>
+                                                                                </td>
                                                                                 <td class="px-6 py-4 whitespace-nowrap">
-                                                                                    Mark</td>
-
+                                                                                    {{ $bathType }}
+                                                                                </td>
                                                                             </tr>
+                                                                        @endforeach
+                                                                        @foreach ($bathComplements as $bathComplement)
                                                                             <tr
                                                                                 class="transition duration-300 ease-in-out border-b hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
                                                                                 <td
                                                                                     class="px-6 py-4 font-medium whitespace-nowrap">
-                                                                                    2</td>
+                                                                                    <i
+                                                                                        class="text-green-500 fa-solid fa-check"></i>
+                                                                                </td>
                                                                                 <td class="px-6 py-4 whitespace-nowrap">
-                                                                                    Jacob</td>
-
+                                                                                    {{ $bathComplement }}
+                                                                                </td>
                                                                             </tr>
-                                                                            <tr
-                                                                                class="transition duration-300 ease-in-out border-b hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-                                                                                <td
-                                                                                    class="px-6 py-4 font-medium whitespace-nowrap">
-                                                                                    3</td>
-                                                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                                                    Larry</td>
-
-                                                                            </tr>
-                                                                        </tbody>
-                                                                    </table>
-                                                                </div>
+                                                                        @endforeach
+                                                                    </tbody>
+                                                                </table>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    </p>
                                                 </div>
+                                                <br />
+                                                <p class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
+                                                    {{ $extraServices }}
+                                                </p><br />
+                                                <p class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
+                                                    {{ $notes }}
+                                                </p><br />
+
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </section>
-                        </div>
+                            </div>
+                        </section>
                     </div>
-                    <!--Modal footer-->
-                    <div
-                        class="flex flex-wrap items-center justify-end flex-shrink-0 p-4 border-t-2 border-opacity-100 rounded-b-md border-neutral-100 dark:border-opacity-50">
-                        <x-secondary-button type='reset' data-te-modal-dismiss data-te-ripple-init
-                            data-te-ripple-color="light">
-                            {{ __('Fechar') }}
-                        </x-secondary-button>
+                </div>
+                <!--Modal footer-->
+                <div
+                    class="flex flex-wrap items-center justify-end flex-shrink-0 p-4 border-t-2 border-opacity-100 rounded-b-md border-neutral-100 dark:border-opacity-50">
+                    <x-secondary-button data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
+                        {{ __('Fechar') }}
+                    </x-secondary-button>
 
 
-                    </div>
-                </form>
+                </div>
+
 
             </div>
         </div>
